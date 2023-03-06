805ca1721490724bbcd0addf7cede0e2320eaa1c
Teaghan O'Briain <obriaintb@gmail.com>
2023-03-06 01:31:56 +0000

    test on all

--- test_starnet_ss.py
+++ test_starnet_ss.py
@@ -158,7 +158,7 @@ plot_progress(losses, model.module.tasks,
 plot_val_MAEs(losses, multimodal_keys+unimodal_keys, 
               y_lims=[(0.,600.), (0.,0.6), (0.,1.0), (0.,.4), (0,40)],
              savename=os.path.join(figs_dir, '%s_val_progress.png'%model_name))
-
+'''
 # Predict on source
 (tgt_mm_labels, tgt_um_labels, 
  pred_mm_labels, pred_um_labels) = predict_labels(model, source_val_dataset, 
@@ -174,18 +174,28 @@ np.save(os.path.join(results_dir, '%s_source_mm_tgts.npy'%model_name), tgt_mm_la
 plot_resid_violinplot(multimodal_keys, tgt_mm_labels, pred_mm_labels,
                       y_lims=[1000, 1.2, 1.5, 0.8], 
                       savename=os.path.join(figs_dir, '%s_source_val_results.png'%model_name))
-
+'''
 # Predict on target
 (tgt_mm_labels, tgt_um_labels, 
- pred_mm_labels, pred_um_labels) = predict_labels(model, target_val_dataset, 
+ pred_mm_labels, pred_um_labels) = predict_labels(model, target_train_dataset, 
                                                   device=device, take_mode=False, 
                                                   combine_batch_probs=True,
                                                  chunk_indices=torch.tensor(chunk_indices),
                                                 chunk_weights=torch.tensor(chunk_weights))
 
+(tgt_mm_labels2, tgt_um_labels2, 
+ pred_mm_labels2, pred_um_labels2) = predict_labels(model, target_val_dataset, 
+                                                  device=device, take_mode=False, 
+                                                  combine_batch_probs=True,
+                                                 chunk_indices=torch.tensor(chunk_indices),
+                                                chunk_weights=torch.tensor(chunk_weights))
+
+pred_mm_labels = np.vstack((pred_mm_labels, pred_mm_labels2))
+tgt_mm_labels = np.vstack((tgt_mm_labels, tgt_mm_labels2))
+
 # Save predictions
-np.save(os.path.join(results_dir, '%s_target_mm_preds.npy'%model_name), pred_mm_labels)
-np.save(os.path.join(results_dir, '%s_target_mm_tgts.npy'%model_name), tgt_mm_labels)
+np.save(os.path.join(results_dir, '%s_target_mm_preds_all.npy'%model_name), pred_mm_labels)
+np.save(os.path.join(results_dir, '%s_target_mm_tgts_all.npy'%model_name), tgt_mm_labels)
 
 # Save a plot
 if len(np.unique(tgt_mm_labels[:,0]))<40:
