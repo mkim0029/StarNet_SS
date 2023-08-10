a73bdad7721ddc57c4f0dc127d4578ebbfd8f732
Teaghan O'Briain <obriaintb@gmail.com>
2023-08-10 23:12:24 +0000

    survey datasets

--- compare_starnet.py
+++ compare_starnet.py
@@ -22,6 +22,22 @@ for i in range(1, 148):
         try:
             # Try loading model
             checkpoint = torch.load(model_filename, map_location=lambda storage, loc: storage)
+            
+            model_name = model_filename.split('/')[-1].split('.')[0]
+            
+            # Model configuration
+            config = configparser.ConfigParser()
+            config.read(config_dir+model_name+'.ini')
+
+            losses = dict(checkpoint['losses'])
+            src_loss = np.mean(losses['val_source_loss'][-3:])
+            tgt_loss = np.mean(losses['val_target_loss'][-3:])
+
+            models_compare.append([i, src_loss, tgt_loss])
+            print('Model %i: %s' % (i, config['Notes']['comment']))
+            print('\tBatch iters: %i' % (losses['batch_iters'][-1]))
+            print('\tSrc Loss: %0.6f' % (src_loss))
+            print('\tTgt Loss: %0.6f' % (tgt_loss))
         except:
             print('Model %i broken. Rerunning' %i)    
             '''
@@ -35,22 +51,6 @@ for i in range(1, 148):
             os.system(cmd)
             '''
             continue
-
-        model_name = model_filename.split('/')[-1].split('.')[0]
-            
-        # Model configuration
-        config = configparser.ConfigParser()
-        config.read(config_dir+model_name+'.ini')
-
-        losses = dict(checkpoint['losses'])
-        src_loss = np.mean(losses['val_source_loss'][-3:])
-        tgt_loss = np.mean(losses['val_target_loss'][-3:])
-
-        models_compare.append([i, src_loss, tgt_loss])
-        print('Model %i: %s' % (i, config['Notes']['comment']))
-        print('\tBatch iters: %i' % (losses['batch_iters'][-1]))
-        print('\tSrc Loss: %0.6f' % (src_loss))
-        print('\tTgt Loss: %0.6f' % (tgt_loss))
 
 models_compare = np.array(models_compare)
 print('Model %i performed the best at predicting source labels with %0.5f' % (models_compare[np.nanargmin(models_compare[:,1]),0], models_compare[np.nanargmin(models_compare[:,1]),1]))
