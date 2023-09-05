59efd7461a71985c58efa4cda95e280a7c9177bf
Teaghan O'Briain <obriaintb@gmail.com>
2023-09-05 16:24:59 +0000

    cnv2 testing

--- compare_cnv2.py
+++ compare_cnv2.py
@@ -14,7 +14,7 @@ for i in range(4, 55):
     model_name = 'starnet_cnv2_%i'%i
 
     model_filenames =  glob.glob(os.path.join(model_dir,model_name+'_*.pth.tar'))
-    model_filenames = [fn if 'lp' not in fn for fn in model_filenames]
+    model_filenames = [fn for fn in model_filenames if 'lp' not in fn]
             continue
     if len(model_filenames)==0:
         model_filenames =  [os.path.join(model_dir,model_name+'.pth.tar')]
